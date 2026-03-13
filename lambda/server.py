fb4c8fbf551736ee73c4f653eb9586daa1d5c0b1
Gonzalo's Macbook <gonzalo.bartual.fernandez2@gmail.com>
2026-03-13 20:58:02 +0100

    Server moved to raspberry pi 3

--- lambda/server.py
+++ lambda/server.py
@@ -1,17 +1,13 @@
-from flask import Flask
-from flask_ask_sdk.skill_adapter import SkillAdapter
-from lambda_function import sb
+from flask import Flask, request, jsonify
+from lambda_function import lambda_handler
 
 app = Flask(__name__)
-skill_adapter = SkillAdapter(
-    skill=sb.create(),
-    skill_id="amzn1.ask.skill.ead66c8d-e209-4b1a-8d3b-1dc7b4580d29",
-    app=app
-)
 
 @app.route('/alexa', methods=['POST'])
 def alexa_skill():
-    return skill_adapter.dispatch_request()
+    # Calling the lambda_handler directly avoids the cryptography library issues
+    response = lambda_handler(request.get_json(), None)
+    return jsonify(response)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=4997)
